0067f6b55d1d82cbbca8554b7668c3e9568fb62c
Sorin-Alexandru Galetuse <5gales10@solent.ac.uk>
2021-09-30 16:38:56 +0100

    modified the program accordinglly the task

--- string_operator.py
+++ string_operator.py
@@ -1,12 +1,12 @@
 print("Please enter the number of lives.")
-lives = input()
+lives = int(input())
 print("Please enter the energy level.")
-energy = input()
+energy = int(input())
 print("Please enter the shield level.")
-shield = input()
+shield = int(input())
 
 print("Health has been set.")
 
-print(f"Lives:  {lives}")
-print(f"Energy:   {energy}")
-print(f"Shield:   {shield}")
+print(f"Lives:  {'♥'*lives}")
+print(f"Energy:   {'♦'*energy}")
+print(f"Shield:   {'♦'*3shield}")
