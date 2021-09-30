74de373b13970bc43eb1f67ed93dc31e8e7fef18
Sorin-Alexandru Galetuse <5gales10@solent.ac.uk>
2021-09-30 16:31:42 +0100

    string_operators

---
+++ string_operator.py
@@ -0,0 +1,12 @@
+print("Please enter the number of lives.")
+lives = input()
+print("Please enter the energy level.")
+energy = input()
+print("Please enter the shield level.")
+shield = input()
+
+print("Health has been set.")
+
+print(f"Lives:  {lives}")
+print(f"Energy:   {energy}")
+print(f"Shield:   {shield}")
