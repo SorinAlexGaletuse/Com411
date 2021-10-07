faeabb6fea18096ae87315e70223d92425f6a8a9
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-07 16:57:48 +0100

    and

---
+++ basic/decisions/and_operator.py
@@ -0,0 +1,12 @@
+print("What did i hear?")
+hear = input()
+print("What did i see?")
+see = input()
+
+if hear == 'grrr' and see == 'two red eyes':
+    print("There is a scary creature. I should get out of there!")
+else:
+    print("I am a little scared but i will continue")
+
+print("Program ended!")
+
