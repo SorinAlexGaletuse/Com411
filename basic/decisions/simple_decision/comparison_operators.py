ac1ca5bba42d0c911af734747117a1650bde3d7f
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-07 15:35:31 +0100

    comparison

---
+++ basic/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,11 @@
+print("Please enter the first number.")
+first = int(input())
+print("Please enter the second number.")
+second = int(input())
+
+if first > second:
+    print("The second number is the smallest.")
+else:
+    print("The first number is the smallest.")
+
+print("Program ended!")
