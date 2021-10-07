a31665291e09c5fce9099e29192931a05ac9441c
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-07 16:04:37 +0100

    nested

---
+++ basic/decisions/simple_decision/nested.py
@@ -0,0 +1,15 @@
+print("What type of cover does the book have?")
+cover = input()
+
+if cover == "soft":
+    print("Is the book perfect-bound?")
+    form = input()
+    if form == "yes":
+        print("Soft cover, perfect bound books are very popular!")
+    else:
+        print("Soft covers with coils or stitches are great for short books")
+
+elif cover == "hard":
+    print("Books with hard covers can be more expensive!")
+
+print("Program ended!")
