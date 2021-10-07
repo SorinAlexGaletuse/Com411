51e9d821f84d2cc8a25d2a2c458440c73d62e739
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-07 17:24:57 +0100

    simple

---
+++ basic/repetitions/while_loop/simple.py
@@ -0,0 +1,9 @@
+print("How many cables should i remove?")
+number = int(input())
+number = 0
+while number <10:
+    print("Removed cable.")
+    number = number+1
+
+print(f"Completed removed the cables.")
+
