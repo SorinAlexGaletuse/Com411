4f2408af2648bc49e7d8aa4b1f8d1f39bd050e8f
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-07 16:51:47 +0100

    or

---
+++ basic/decisions/or_operator.py
@@ -0,0 +1,13 @@
+print("What type of adventure should i have?")
+adventure = input()
+if adventure =='scary' or adventure =='short':
+    print("Entering the dark forest!")
+elif adventure == 'safe' or adventure == 'long':
+    print("Taking the safe route!")
+else:
+    print("Not sure which route to take.")
+
+print("Program ended!")
+
+
+
