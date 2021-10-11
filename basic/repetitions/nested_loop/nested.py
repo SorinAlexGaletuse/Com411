8e7b95bc48bb8e745acdd5f15c3e890617014eeb
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-11 18:51:41 +0100

    Nested_for

---
+++ basic/repetitions/nested_loop/nested.py
@@ -0,0 +1,13 @@
+print("How many rows should i have?")
+rows = int(input())
+print("How many columns should i have?")
+columns = int(input())
+
+print("Here i go:\n")
+
+for count in range(0,rows,1):
+    for count2 in range(0,columns,1):
+        print(":-)  ",end="")
+    print()
+
+print("\nDone")
