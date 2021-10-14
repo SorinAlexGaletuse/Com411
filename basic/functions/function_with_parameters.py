6005bfb5b62125dd360d99f873d4b7fbba2a37db
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-14 16:03:08 +0100

    parameters

---
+++ basic/functions/function_with_parameters.py
@@ -0,0 +1,9 @@
+def climb_ladder(untill,too_go):
+
+    if untill > too_go :
+        print("Still some way to go!")
+    else:
+        print("We are almost there!")
+
+climb_ladder(5,2)
+climb_ladder(2,5)
