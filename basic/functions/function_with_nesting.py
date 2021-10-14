33c301f02f125e6debbee204e0d0b9a1d13f927e
SorinAlexandruGaletuse <5gales10@solent.ac.uk>
2021-10-14 15:33:04 +0100

    f with nesting

---
+++ basic/functions/function_with_nesting.py
@@ -0,0 +1,11 @@
+def identify():
+    print("Hey what is there?")
+    see = input()
+
+    if see == 'a large boulder':
+        print("It's time to run!")
+
+    else:
+        print("We will be fine.")
+
+identify()
