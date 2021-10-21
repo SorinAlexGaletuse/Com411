68c7ac4f00a0bf4f7fa8e4e4dca0487ca2363ae2
SorinAlexGaletuse <5gales10@solent.ac.uk>
2021-10-21 16:31:14 +0100

    readcsv

---
+++ data/files/csv/read_csv.py
@@ -0,0 +1,19 @@
+import csv
+
+def read(file_path):
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+        headings = next(csv_reader)
+        print(f"Headings:\n{headings}")
+        print("Values:")
+        for remaing_line in csv_reader:
+            print(remaing_line)
+
+def run():
+    read("bots.csv")
+
+if __name__ == "__main__":
+    run()
+
+
+
