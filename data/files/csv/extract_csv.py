51af55cdccd5a8f04a6d42e6a5c9fba0300e791a
SorinAlexGaletuse <5gales10@solent.ac.uk>
2021-10-21 16:52:25 +0100

    extract

---
+++ data/files/csv/extract_csv.py
@@ -0,0 +1,17 @@
+import csv
+
+def extract(file_path):
+    print("Extrating...")
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        names =""
+        print("Done! The extracted names are as follows:")
+        for value in csv_reader:
+            print(f"{value[1]}")
+
+def run():
+    extract("bot.csv")
+
+if __name__ == "__main__":
+    run()
