25c7e36d37410235e9cc9e255f60b85df7a7a83a
Sorin Alex Galetuse <5gales10@solent.ac.uk>
2021-11-05 08:56:26 +0000

    dict

---
+++ data/dicts/iterate_dict.py
@@ -0,0 +1,29 @@
+def pattern():
+    sequences = {"Short Sequence":3,"Medium Sequence":2,"Long Sequence":1}
+    return sequences
+
+def display_keys(data):
+    print("Keys:")
+    for key in data:
+        print(key)
+    print()
+def display_values(data):
+    print("Values:")
+    for value in data:
+        print(value)
+    print()
+def display_items(data):
+    print("Items:")
+    for key, value in data:
+        print(f"{key}: {value}")
+    print()
+
+def run():
+    data = pattern()
+    print(f"Dictionary:\n {data}\n")
+
+    display_keys(data)
+    display_values(data)
+    display_items(data)
+
+run()
---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,9 @@
+def pattern():
+    sequences = {"Shoet Sequence":3,"medium Sequence":2,"Long Sequence":1}
+    return sequences
+
+def run():
+    print(pattern())
+
+if __name__ == "__main__":
+    run()
