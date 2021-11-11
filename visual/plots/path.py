b07bd5fff4ff74dafb85642154f14a12b3b17e3e
Sorin Alex Galetuse <5gales10@solent.ac.uk>
2021-11-11 15:40:14 +0000

    path code done

--- visual/plots/path.py
+++ visual/plots/path.py
@@ -1,5 +1,28 @@
+import matplotlib.pyplot as plt
+
+
 def coordinate():
-    
+    print("Please enter a value for X:")
+    x = int(input())
+    print("Please enter a value for Y:")
+    y = int(input())
+    return (x,y)
+
 def path():
+    print("Retrieving path...")
+    x_value = []
+    y_value = []
+    for count in range(4):
+        data = coordinate()
+        x_value.append(data[0])
+        y_value.append(data[1])
+    return [x_value,y_value]
 
 def run():
+    values = path()
+    plt.plot(values[0],values[1],'r--o')
+    plt.xlabel("X Values")
+    plt.ylabel("Y Values")
+    plt.show()
+
+run()
