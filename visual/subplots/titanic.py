07197300707acc8f53bde2d203c99ce7074905b3
SorinAlexGaletuse <5gales10@solent.ac.uk>
2021-11-18 16:29:48 +0000

    ploted a comparation of data.. looks horible

---
+++ visual/subplots/titanic.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import csv
+
+def read_data():
+    titanic = {"survived":[], "sex":[], "age":[], "fare":[]}
+
+    with open("C:/Users/user/PycharmProjects/Com411/visual/subplots/titanic.csv") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for line in csv_reader:
+            survived = line[1].strip()
+            sex = line[14].strip()
+            age = line[4].strip()
+            fare = line[8].strip()
+            if(survived!="" and sex!="" and age!="" and fare!=""):
+                titanic["survived"].append(bool(int(survived)))
+                if (int(sex)==0):
+                    titanic["sex"].append("male")
+                else:
+                    titanic["sex"].append("female")
+
+                titanic["age"].append(float(age))
+                titanic["fare"].append(round(float(fare),2))
+    return titanic
+
+def run():
+    data = read_data()
+    fig, axs = plt.subplots(2,2)
+    axs[0,0].bar(data["survived"],data["age"])
+    axs[0,1].scatter(data["survived"],data["fare"])
+    axs[1,0].step(data["fare"],data["age"])
+    axs[1,1].plot(data["sex"],data["survived"])
+    plt.show()
+
+run()
